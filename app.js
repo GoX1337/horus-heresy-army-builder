3f1d4f684452385b2ccbb7310ec09438cc0098ff
gox <gox.roche@gmail.com>
2022-06-15 20:00:39 +0200

    Rename main routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 const app = express();
 const authRoute = require('./auth-route');
 const auth = require('./auth');
-const builderRoutes = require('./builder-routes');
+const hhBuilderRoutes = require('./hh-builder-routes');
 const port = process.env.PORT || 8080;
 
 app.disable('x-powered-by');
@@ -17,7 +17,7 @@ app.use(express.static('dist'));
 app.use(morgan('combined'));
 
 app.use("/auth", authRoute);
-app.use("/api", auth, builderRoutes);
+app.use("/api", auth, hhBuilderRoutes);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}...`);
